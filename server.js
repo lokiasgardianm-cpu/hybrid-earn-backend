70d85ce717a645c5c471959759302659ad18a3d5
lokiasgardianm-cpu <loki.asgardian.m@gmail.com>
2026-02-19 23:11:44 +0600

    Added tap route and supabase client

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const cors = require("cors");
 const { createClient } = require("@supabase/supabase-js");
 
 const supabase = createClient(
-  process.env.SUPABASE_URL,
+  process.env.DATABASE_URL,
   process.env.SUPABASE_SERVICE_ROLE_KEY
 );
 
